62db4b302b4c49faab7f1def0bf344fab4785743
rkgupta032 <rkitbhugupta@gmail.com>
2018-11-09 09:53:49 +0530

    Update AVLOperations.java
    added insert operation in avl  height balance tree

--- DataStructure/src/rk/org/tree/avl/AVLOperations.java
+++ DataStructure/src/rk/org/tree/avl/AVLOperations.java
@@ -5,10 +5,31 @@ import rk.org.tree.BinaryTreeQuestions;
 
 public class AVLOperations {
 	
-	public AVLTreeNode insertInAVLTree(AVLTreeNode root,AVLTreeNode parent,int data){
-		
-		
-		return null;
+	public AVLTreeNode insertInAVLTree(AVLTreeNode root,int data){
+		if(root == null){
+			AVLTreeNode newNode = new AVLTreeNode(data);
+			newNode.setHeight(0);
+			root = newNode;
+		}
+		else if(data < root.getData()){
+				root.setLeft(insertInAVLTree(root.getLeft,data));
+				if(Math.abs(root.getLeft().height - root.getRight().height) > 1){
+					if(data < root.getLeft().data)
+						SingleRotateLeft(root.getLeft());
+					else
+						DoubleRotateLeft(root.getLeft());
+				
+				}
+			}
+			else {
+				root.setRight(insertInAVLTree(root.getRight(),data));
+				if(Math.abs(root.getLeft().height -root.getRight().height) > 1)
+					if(data > root.getright().data)
+						SingleRotateRight(root.getRight());
+					else
+						DoubleRotateRight(root.getRight());
+			}
+		return root;
 	}
 	
 	
